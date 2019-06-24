a04198c771412e40e38ea6cdd2c0c4a60735d5a2
mentesuprema <mentesuprema@localhost>
2019-06-24 08:45:44 -0400

    Aterado o ide

--- CadastroSocial/src/br/cadastrosocial/model/Funcionario.java
+++ CadastroSocial/src/br/cadastrosocial/model/Funcionario.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
